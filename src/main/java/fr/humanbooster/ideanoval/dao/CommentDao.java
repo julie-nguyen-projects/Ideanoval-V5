ec525939ddb438c49eb56c44f696e086dca65466
neamesis <Poupipoupi89>
2016-11-19 11:26:10 +0100

    Comment : getAllComments and display on idea page

--- src/main/java/fr/humanbooster/ideanoval/dao/CommentDao.java
+++ src/main/java/fr/humanbooster/ideanoval/dao/CommentDao.java
@@ -3,10 +3,14 @@ package fr.humanbooster.ideanoval.dao;
 import fr.humanbooster.ideanoval.business.Comment;
 import fr.humanbooster.ideanoval.business.User;
 
+import java.util.List;
+
 /**
  * Created by Julie on 19/11/2016.
  */
 public interface CommentDao {
 
     public void addComment(Comment comment);
+
+    public List<Comment> getAllComments();
 }
